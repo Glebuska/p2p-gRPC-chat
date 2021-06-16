b7edb8955f04914a79783ad1c8122a7db5244d7a
Simon Tsirikov <tsirikov.s.a@gmail.com>
2021-06-16 08:59:52 +0300

    replace generated files with script

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Generating proto grpc files..."
-python -m grpc_tools.protoc -I=src/resources --python_out=src/resources --grpc_python_out=src/resources src/resources/chat.proto
+python3 -m grpc_tools.protoc -I=src/resources --python_out=src/resources --grpc_python_out=src/resources src/resources/chat.proto
 echo "DONE"
