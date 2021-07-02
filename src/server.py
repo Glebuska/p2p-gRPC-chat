fd3ded1cfd62342fa366a2ae460f72ca9a3f2798
Simon Tsirikov <tsirikov.s.a@gmail.com>
2021-07-02 13:34:54 +0300

    modify monitoring behaviour

--- src/server.py
+++ src/server.py
@@ -1,5 +1,6 @@
 import resources.chat_pb2 as chat
 import resources.chat_pb2_grpc as rpc
+from time import sleep
 
 
 class ChatServer(rpc.ServerServicer):
@@ -11,11 +12,16 @@ class ChatServer(rpc.ServerServicer):
     def ChatStream(self, request_iterator, context):
         """Return a last message index."""
         lastindex = 0
+        delay = 0.5
         while True:
-            if len(self.chats) > lastindex:
+            while len(self.chats) > lastindex:
+                delay = 0.5
                 n = self.chats[lastindex]
                 lastindex += 1
                 yield n
+            delay = min(delay * 2, 16)
+            sleep(delay)
+            
 
     def SendNote(self, request: chat.MyMessage, context):
         """Handle a sent message."""
